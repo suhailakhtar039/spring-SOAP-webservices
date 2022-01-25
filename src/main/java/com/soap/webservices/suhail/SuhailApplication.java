ba322898e836c34090734a10d2c3aa02a283c093
Suhail Akhtar <suhailakhtar039@gmail.com>
2022-01-26 00:27:24 +0530

    starting soap webservices

---
+++ src/main/java/com/soap/webservices/suhail/SuhailApplication.java
@@ -0,0 +1,13 @@
+package com.soap.webservices.suhail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuhailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuhailApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/soap/webservices/suhail/SuhailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soap.webservices.suhail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuhailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
